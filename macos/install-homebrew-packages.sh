1f7027531b461b83098d520e3e83fb1dbb74c82a
Brendon McGay <dev@brendonn.com>
2025-03-10 16:15:23 -0400

    brew

--- macos/install-homebrew-packages.sh
+++ macos/install-homebrew-packages.sh
@@ -7,7 +7,7 @@ if [[ "$(uname)" == "Darwin" ]] && command -v brew >/dev/null 2>&1; then
   export HOMEBREW_NO_AUTO_UPDATE=1
   export HOMEBREW_NO_INSTALL_UPGRADE=1
   export HOMEBREW_BUNDLE_DUMP_NO_VSCODE=1
-  export HOMEBREW_BUNDLE_FILE=$HOME/.Brewfile
+  export HOMEBREW_BUNDLE_FILE=$HOME/Brewfile
   brew bundle -q --no-upgrade --file ~/.Brewfile
 
   echo "All packages installed."
