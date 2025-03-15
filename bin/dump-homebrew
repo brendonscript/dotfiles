82c2c48a7e0f4277994433576014d5d894396092
Brendon McGay <dev@brendonn.com>
2025-03-15 17:45:27 -0400

    chore: migrate bin files to fish

--- bin/dump-homebrew
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Dumping Homebrew packages..."
-brew bundle dump --force --global
--- config/fish/conf.d/mac.fish
+++ config/fish/conf.d/mac.fish
@@ -1,4 +1,23 @@
 if test (uname) = Darwin
+
+    function install_homebrew_packages
+        if test (uname) = Darwin; and type -q brew
+            echo "Installing Homebrew Packages..."
+
+            # Export environment variables
+            set -gx HOMEBREW_NO_AUTO_UPDATE 1
+            set -gx HOMEBREW_NO_INSTALL_UPGRADE 1
+            set -gx HOMEBREW_BUNDLE_DUMP_NO_VSCODE 1
+            set -gx HOMEBREW_BUNDLE_FILE $HOME/Brewfile
+
+            # Run brew bundle
+            brew bundle -q --no-upgrade --file ~/Brewfile
+
+            echo "All packages installed."
+            return 0
+        end
+    end
+
     fish_add_path /opt/homebrew/opt/llvm/bin
     fish_add_path /Applications/Emacs.app/Contents/MacOS/bin
     fish_add_path /opt/homebrew/sbin
@@ -10,9 +29,10 @@ if test (uname) = Darwin
     set -gx HOMEBREW_BUNDLE_MAS_SKIP "Xcode Keynote Numbers 'Okta Verify' iMovie GarageBand Mapper Pages"
     set -gx HOMEBREW_BUNDLE_FILE "$HOME/Brewfile"
 
-    abbr brewup update-homebrew
-    abbr brewdump dump-homebrew
-    abbr brewdot update-dotfiles-homebrew
+    abbr brewup "brew update && brew upgrade"
+    abbr brewdump "brew bundle dump --force --global"
+    abbr brewdot "brew bundle dump --force"
+    abbr brewinstall install_homebrew_packages
 
     if type -q shortcuts
         abbr --add show-menu 'shortcuts run "Show Menubar"'
