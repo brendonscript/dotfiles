a477a38ccaded80363617c54c12c3ab17c98c0d6
Brendon McGay <dev@brendonn.com>
2025-03-15 18:23:41 -0400

    fix: restore toggle mac scripts

---
+++ macos/toggle-dock.sh
@@ -0,0 +1,1 @@
+osascript -e 'tell application "System Events" to tell dock preferences to set autohide to not autohide'
---
+++ macos/toggle-menubar.sh
@@ -0,0 +1,1 @@
+osascript -e 'tell application "System Events" to tell dock preferences to set autohide menu bar to not autohide menu bar'
