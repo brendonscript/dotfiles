bbdaf9312be194dacc0680c85ab9f0c0728eb9df
Brendon McGay <dev@brendonn.com>
2025-04-02 18:35:18 -0400

    refactor: windows

--- windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,25 @@
 $configDirectory = "$HOME\Documents\Powershell"
-$ENV:EDITOR = "code"
-$ENV:VISUAL = "code"
+$ENV:EDITOR = "nvim"
+$ENV:VISUAL = "nvim"
 # $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin;C:\Program Fiels\Git\usr\bin"
 $env:Path += ";C:\Program Fiels\Git\usr\bin"
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+$env:XDG_CONFIG_HOME = "$HOME\.config"
 
 . "$configDirectory\Aliases.ps1"
 . "$configDirectory\CustomModules.ps1"
 . "$configDirectory\Zoxide.ps1"
 . "$configDirectory\Completions\Wezterm.ps1"
+
+Remove-Item Alias:"cat"
+Remove-Item Alias:"cp"
+Remove-Item Alias:"dir"
+Remove-Item Alias:"echo"
+Remove-Item Alias:"ls"
+Remove-Item Alias:"mv"
+Remove-Item Alias:"pwd"
+Remove-Item Alias:"rm"
+Remove-Item Alias:"rmdir"
 
 # Set-PSReadLineOption -EditMode vi
 # Set-PSReadLineKeyHandler -Key 'j,k' -ViMode Insert -Function ViCommandMode
@@ -29,3 +40,4 @@ function OnViModeChange {
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
 
 Invoke-Expression (&starship init powershell)
+
